c7506c1338292cd17ccfc5ff891063bd8d67e75e
Elena Semseeva <e.semseeva@yandex.ru>
2023-09-01 14:19:48 +0300

    feat: Functionality Reviews added

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -1,10 +1,13 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.exception.Review.ReviewNotFoundException;
 import ru.yandex.practicum.filmorate.exception.film.FilmNotFoundException;
 import ru.yandex.practicum.filmorate.exception.film.GenreNotFoundException;
 import ru.yandex.practicum.filmorate.exception.film.InvalidReleaseDateException;
@@ -44,6 +47,12 @@ public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleMpaNotFoundException(final MpaNotFoundException exception) {
+        return new ErrorResponse(exception.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleReviewNotFoundException(final ReviewNotFoundException exception) {
         return new ErrorResponse(exception.getMessage());
     }
 
