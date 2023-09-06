ba10d82978f8b5d4d1582b79522b3d2f6477d634
asdqwe <asdaedadasczx@mail.ru>
2023-09-06 22:35:35 +0200

    add remove endpoint

---
+++ src/main/java/ru/yandex/practicum/filmorate/exception/exception/film/MpaNotFoundException.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.filmorate.exception.exception.film;
+
+public class MpaNotFoundException extends RuntimeException {
+    public MpaNotFoundException(String message) {
+        super(message);
+    }
+}
